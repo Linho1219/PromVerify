2f3ae3d4b83d9338c48ef32fb495f135c5d3aff9
Linho <linho1219@outlook.com>
2024-09-27 16:08:18 +0800

    删除多余文件

--- types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-declare global {
-  namespace Express {
-    interface Application {
-      ws: (route: string, callback: (ws: WebSocket, req: express.Request) => void) => void;
-    }
-  }
-}
